9724464d75e0e2fd08f0049a89c9bbf97c895908
ApstK <profking11@gmail.com>
2022-12-23 01:35:24 -0800

    code correction for 101

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,28 +1,37 @@
-#include <stdio.h>
+#include "main.h"
 
-	/**
-	 * main - define what the main function isfor
-	 * Return: Always 0
-	 */
+/**
+ * print_number - Prints any integer with putchar
+ * @n: Number to prints
+ *
+ * Return: Nothing
+ */
 
-	int main(void)
+void print_number(int n)
 {
-	int n;
-	int a[5];
-	int *p;
+	unsigned int m, d, count;
 
-	a[2] = 1024;
-	p = &n;
-  /*
-   * write your line of code here...
-   * Remember:
-   * - you are not allowed to use a
-   * - you are not allowed to modify p
-   * - only one statement
-   * - you are not allowed to code anything else than this line of code
-   */
-	p[5] = 98;
-  /* ...so that this prints 98\n */
-	printf("a[2] = %d\n", a[2]);
-	return (0);
+	if (n < 0)
+	{
+		_putchar('-');
+		m = n * -1;
+	}
+	else
+	{
+		m = n;
+	}
+
+	d = m;
+	count = 1;
+
+	while (d > 9)
+	{
+		d /= 10;
+		count *= 10;
+	}
+
+	for (; count >= 1; count /= 10)
+	{
+		_putchar(((m / count) % 10) + 48);
+	}
 }
